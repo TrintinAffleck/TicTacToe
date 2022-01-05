66affa25508ae2709d62a2d7a4a9c98b4a72618c
Trintin Affleck <t.affleck101@hotmail.com>
2022-01-04 18:17:23 -0700

    Win conditions added, refactored player_input(), bug fixes

--- TicTacToe/TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe/TicTacToe.py
@@ -3,10 +3,12 @@ game_list = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 game_on = True
 
 def clear():
+    #To clear the screen
     print("\n"*100)
 
 def display(game_list: list):
     print("Here is the current list")
+    #Printing list into 3x3 rows AKA TicTacToe board
     print(game_list[7] + '|' + game_list[8] + '|' + game_list[9])
     print(game_list[4] + '|' + game_list[5] + '|' + game_list[6])
     print(game_list[1] + '|' + game_list[2] + '|' + game_list[3])
@@ -14,23 +16,34 @@ def display(game_list: list):
 
 def player_input():
 
-    marker = ' '
-
-    while marker != 'X' or 'O':
+    marker = " "
+    #If players input is not x or o keep asking for input
+    while marker not in ['X','x','O','o']:
         marker = input("Please choose your marker (X or O)")
-
+        
     player1 = marker
 
-    if player1 == 'X':
+    #Setting player markers
+    if player1 in ['X','x']:
+        print("You chose X as your marker")
+        player1 = 'X'
         player2 = 'O'
     else:
+        print("You chose O as your marker")
+        player1 = 'O'
         player2 = 'X'
 
     return (player1,player2)
 
+def place_marker(board, marker: str, position: int):
+    
+    board = game_list
+    board[position] = marker
+
+    return game_list
 
 
-def position_choice():
+def position_choice(position: int):
 
     choice = 'wrong'
 
@@ -40,16 +53,39 @@ def position_choice():
 
         if choice not in range(1-10):
             print("Sorry, invalid choice!")
-
-    return int(choice)
+    position = int(choice)
+    return position
 
 def replacement_choice(game_list, position):
     user_placement = input("Type a string to place at position: ")
 
-    #position = position_choice()
     game_list[position] = user_placement
-    print(game_list)
     return game_list
+
+def win_check(board, mark):
+    board = game_list
+    mark = player_input()
+    playerwon = False
+    #Checking for X or O's in a row
+    if board[1] == board[2] == board[3]: #BottomRow
+        playerwon = True
+    if board[1] == board[4] == board[7]: #LeftSide
+        playerwon = True
+    if board[3] == board[6] == board[9]: #RightSide
+        playerwon = True
+    if board[7] == board[8] == board[9]: #TopRow
+        playerwon = True
+    if board[7] == board[5] == board[3]: #LeftDiagonal
+        playerwon = True
+    if board[9] == board[5] == board[1]: #RightDiangonal
+        playerwon = True
+    if board[4] == board[5] == board[6]: #HorizonalRow
+        playerwon = True
+    if board[2] == board[5] == board[8]: #VerticalRow
+        playerwon = True
+    
+
+
 
 def gameon_choice():
 
@@ -85,6 +121,7 @@ while game_on:
 
     display(game_list)
     player_input()
+    place_marker(game_list,'S',1)
 
     #position = position_choice()
 
