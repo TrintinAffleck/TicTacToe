d4013e4df58f90ef35cc66af2a923886ff09ecc1
Trintin Affleck <t.affleck101@hotmail.com>
2022-01-06 17:46:26 -0700

    Cleaned up player_input, place_marker and choose_first.

--- TicTacToe/TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe/TicTacToe.py
@@ -1,48 +1,70 @@
 import random
 
 #Variables
-game_list = [' ','X',' ',' ',' ',' ',' ',' ',' ',' ']
+game_list = [' ']*10
 game_on = True
 
 def clear():
     #To clear the screen
-    print("\n"*100)
+    print("\n"*24)
 
-def display(game_list: list):
-    print("Here is the current list")
+def display(board):
     #Printing list into 3x3 rows AKA TicTacToe board
-    print(game_list[7] + '|' + game_list[8] + '|' + game_list[9])
-    print(game_list[4] + '|' + game_list[5] + '|' + game_list[6])
-    print(game_list[1] + '|' + game_list[2] + '|' + game_list[3])
+    clear()
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print("Here is the current list")
     
-
 def player_input():
+    ''' 
+    OUTPUT
+    (Player1 Marker, Player2 Marker)
+    '''
+    marker = ""
+    #While not X or O keep asking for input
+    while marker != 'X' and marker != 'O':
+        marker = input("Player1: Choose Marker (X or O) ").upper()
 
-    marker = " "
-    #If players input is not x or o keep asking for input
-    while marker not in ['X','x','O','o']:
-        marker = input("Please choose your marker (X or O)")
-        
-    player1 = marker
+    if marker == 'X':
+        return ('X','O')
+    return ('O','X')
 
-    #Setting player markers
-    if player1 in ['X','x']:
-        print("You chose X as your marker")
-        player1 = 'X'
-        player2 = 'O'
-    else:
-        print("You chose O as your marker")
-        player1 = 'O'
-        player2 = 'X'
-
-    return (player1,player2)
 
 def place_marker(board, marker: str, position: int):
-    
     board = game_list
     board[position] = marker
 
-    return game_list
+    return board
+
+def win_check(board, mark: str):
+    #Checking for X or O's in a row
+    return (board[1] == board[2] == board[3] == mark or #BottomRow
+    board[1] == board[4] == board[7] == mark or #LeftSide
+    board[3] == board[6] == board[9] == mark or #RightSide
+    board[7] == board[8] == board[9] == mark or #TopRow
+    board[7] == board[5] == board[3] == mark or #LeftDiagonal
+    board[9] == board[5] == board[1] == mark or #RightDiangonal
+    board[4] == board[5] == board[6] == mark or #HorizonalRow
+    board[2] == board[5] == board[8] == mark) #VerticalRow
+
+    if playerwon == False:
+        print("Its a Tie!")
+        gameon_choice()
+
+def choose_first():
+    rand = random.randint(1,2)
+    if rand == 1:
+        return "Player 1"
+    return "Player 2"
+    
+
+def space_check(board, position: int) -> bool:
+    return board[position] == ' '
+
+def full_board_check(board):
+    board = game_list
+
 
 
 def position_choice(position: int):
@@ -57,44 +79,6 @@ def position_choice(position: int):
             print("Sorry, invalid choice!")
     position = int(choice)
     return position
-
-
-def win_check(board, mark: str):
-    board = game_list
-    mark = player_input()
-    print(mark)
-    playerwon = False
-    #Checking for X or O's in a row
-    if board[1] == board[2] == board[3] == mark: #BottomRow
-        playerwon = True
-    if board[1] == board[4] == board[7] == mark: #LeftSide
-        playerwon = True
-    if board[3] == board[6] == board[9] == mark: #RightSide
-        playerwon = True
-    if board[7] == board[8] == board[9] == mark: #TopRow
-        playerwon = True
-    if board[7] == board[5] == board[3] == mark: #LeftDiagonal
-        playerwon = True
-    if board[9] == board[5] == board[1] == mark: #RightDiangonal
-        playerwon = True
-    if board[4] == board[5] == board[6] == mark: #HorizonalRow
-        playerwon = True
-    if board[2] == board[5] == board[8] == mark: #VerticalRow
-        playerwon = True
-
-    if playerwon == False:
-        print("Its a Tie!")
-        gameon_choice()
-    
-
-def choose_first():
-    return random.randint(1,2)
-    
-def space_check(board, position: int) -> bool:
-    board = game_list
-    if board[position] in ['X', 'O']:
-        return True
-    return False
 
 def gameon_choice():
 
@@ -124,14 +108,16 @@ def user_choice():
             print('You are out of acceptable range. (1-9)')
             continue
         print('Sorry! that was not a digit.')
+    return int(choice)
 
 #Function Calls
 while game_on:
     
-    #display(game_list)
-    #player_input()
-    #place_marker(game_list,'S',1)
+    display(game_list)
+    player_input()
+    place_marker(game_list, 'X' , 2)
     #win_check(game_list," ")
     #gameon_choice()
-    #space_check(game_list,2)
+    #space_check(game_list,1)
+    #full_board_check(game_list)
     #game_on = gameon_choice()
